e57e77a6b785e0ede75b57246a18729c39265d10
MahmudTheGreat <mahmodembaya@gmail.com>
2018-05-02 16:00:16 +0300

    + router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactRethinkdb from 'react-rethinkdb';
-import createReactClass from 'create-react-class';
-import Login from './Login';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactRethinkdb from 'react-rethinkdb'
+import createReactClass from 'create-react-class'
+import Login from './Login'
 import * as Questions from './Questions'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
-let r = ReactRethinkdb.r;
+let r = ReactRethinkdb.r
 
 const App = createReactClass({
 
   render() {
-    return <Login/>
+    return <Router>
+      <div>
+        <Route path="/" component={Login} />
+      </div>
+    </Router>
   },
+})
 
-});
-
-export default App;
+export default App
