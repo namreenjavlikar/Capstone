228c1b7533ecae1384c831509193aec8f0b95d6c
Khalid <you@example.com>
2018-05-02 13:40:47 +0300

    +importing Questions in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import ReactRethinkdb from 'react-rethinkdb';
 import createReactClass from 'create-react-class';
 import Login from './Login';
+import * as Questions from './Questions'
 
 let r = ReactRethinkdb.r;
 
