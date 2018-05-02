67c627a04b8c4965658bece357d5881a52e19ed6
Nazha Nayim <you@example.com>
2018-05-02 14:58:32 +0300

    + forgot password latest

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -2,24 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactRethinkdb from 'react-rethinkdb';
 import createReactClass from 'create-react-class';
+import '../node_modules/uikit/dist/css/uikit.css';
+import './App.css';
+import photo from './photo.png';
+import logo from './logo.png';
+//import noScroll from 'no-scroll'
+// var noScroll = require('no-scroll');
+// noScroll.on();
 let r = ReactRethinkdb.r;
 
-// Open a react-rethinkdb session (a WebSocket connection to the server)
-// ReactRethinkdb.DefaultSession.connect({
-//   host: 'localhost', // hostname of the websocket server
-//   port: 8015,        // port number of the websocket server
-//   path: '/db',       // HTTP path to websocket route
-//   secure: false,     // set true to use secure TLS websockets
-//   db: 'capstone',    // default database, passed to rethinkdb.connect
-// });
+//Open a react-rethinkdb session (a WebSocket connection to the server)
+ReactRethinkdb.DefaultSession.connect({
+  host: 'localhost', // hostname of the websocket server
+  port: 8015,        // port number of the websocket server
+  path: '/db',       // HTTP path to websocket route
+  secure: false,     // set true to use secure TLS websockets
+  db: 'capstone',    // default database, passed to rethinkdb.connect
+});
 
-const App = createReactClass({
+const ForgotPassword = createReactClass({
 
     mixins: [ReactRethinkdb.DefaultMixin],
 
     getInitialState() {
         return {
-            email: '60084089@cna-qatar.edu.qa',
+            email: '',
             errorMessage: ''
         };
     },
@@ -37,14 +44,14 @@ const App = createReactClass({
     async sendEmail() {
 
         //find user with this specific email
-        let query = r.table('users').filter({ email:this.state.email })
+        let query = r.table('users').filter({ email: this.state.email })
 
         let user = {}
         await ReactRethinkdb.DefaultSession.runQuery(query).then(
             (res) => {
-                res.toArray(function(err, results) {
+                res.toArray(function (err, results) {
                     console.log(results);
-                   user = results
+                    user = results
                 });
             })
 
@@ -77,15 +84,30 @@ const App = createReactClass({
 
     render() {
         return (
-            <div>
-                <input type="email" value={this.state.email} placeholder="Enter Your Email" onChange={
-                    (event) => this.setState({ email: event.target.value, error: '' })} />
-                <button onClick={() => this.sendEmail()}>Send email</button>
+            <div className='login-container' style={{
+                backgroundImage: 'url(' + photo + ')',
+                backgroundSize: 'cover',
+                overflow: 'hidden',
+            }}>
+                <center>
+                    <div className="uk-card uk-card-default uk-card-body uk-width-1-4@m  login-card" style={{ borderRadius: 20 }}>
+                        <img src={logo} style={{ width: 200, height: 150 }} />
+                        <hr />
+                        <h2 className="reset-title2"><strong>Enter your email to reset password</strong></h2>
+                        <div className="uk-margin"  >
+                            <div className="uk-inline reset-input">
+                                <input className="uk-input reset-input" type="email" value={this.state.email} placeholder="Enter Your Email" onChange={
+                                    (event) => this.setState({ email: event.target.value, error: '' })} />
+                            </div>
+                            <p>{this.state.errorMessage}</p>
+                        </div>
+                        <button className="uk-button reset-btn" onClick={() => this.sendEmail()}>Send email</button>
 
-                <p>{this.state.errorMessage}</p>
+                    </div>
+                </center>
             </div>
         )
     },
 });
 
-export default App;
+export default ForgotPassword;
