0f8b4ad9fd8c17fc7e9046c02378d1b943d150a4
Namreen <you@example.com>
2018-05-02 13:16:22 +0300

    updated login

---
+++ src/App2.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactRethinkdb from 'react-rethinkdb';
+import createReactClass from 'create-react-class';
+let r = ReactRethinkdb.r;
+
+// Open a react-rethinkdb session (a WebSocket connection to the server)
+ReactRethinkdb.DefaultSession.connect({
+  host: 'localhost', // hostname of the websocket server
+  port: 8015,        // port number of the websocket server
+  path: '/db',       // HTTP path to websocket route
+  secure: false,     // set true to use secure TLS websockets
+  db: 'capstone',    // default database, passed to rethinkdb.connect
+});
+
+const App = createReactClass({
+
+  mixins: [ReactRethinkdb.DefaultMixin],
+
+  getInitialState() {
+    return {
+      name: "",
+      testing: "Khalid"
+    };
+  },
+
+  observe(props, state) {
+    return {
+      users: new ReactRethinkdb.QueryRequest({
+        query: r.table('users'),
+        changes: true,
+        initial: [],
+      }),
+      pets: new ReactRethinkdb.QueryRequest({
+        query: r.table('testing'),
+        changes: true,
+        initial: [],
+      }),
+    };
+  },
+
+  handleSubmit() {
+    let query = r.table('testing').get("44cfd9cf-1510-4977-837f-68b3cba6d52a").update({ name: this.state.name, testing: "hello" });
+    ReactRethinkdb.DefaultSession.runQuery(query);
+    this.setState({ name: '' })
+  },
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.name} onChange={(event) => this.setState({ name: event.target.value })}/>
+        <button onClick={() => this.handleSubmit()}>Submit</button>
+        {
+          this.data.users.value().map((x) => {
+            return <div key={x.id}>{x.name}</div>;
+          })
+        }
+        <br/>
+        <hr/>
+        {
+          this.data.pets.value().map((x) => {
+            return <div key={x.id}>
+                {x.name} {x.address} {x.testing}
+                {/* Pets: {x.pets.map((y) => y.name).join(',')} */}
+            </div>;
+          })
+        }
+      </div>
+    )
+  },
+});
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactRethinkdb from 'react-rethinkdb';
+import createReactClass from 'create-react-class';
+import jwt from 'express-jwt'
+import '../node_modules/uikit/dist/css/uikit.css';
+import './App.css';
+import photo from './photo.png';
+import logo from './logo.png'
+
+import { sign } from 'jsonwebtoken'
+import bcrypt from "bcryptjs"
+
+let r = ReactRethinkdb.r;
+const secret = 'abc'
+let noScroll = require('no-scroll')
+noScroll.on();
+
+ReactRethinkdb.DefaultSession.connect({
+    host: 'localhost',
+    port: 8015,
+    path: '/db',
+    secure: false,
+    db: 'capstone',
+});
+
+const Login = createReactClass({
+
+    mixins: [ReactRethinkdb.DefaultMixin],
+
+    getInitialState() {
+        return {
+            credentials: '',
+            messageToUser: '',
+            showcredentials: false
+        };
+    },
+
+    observe(props, state) {
+        return {
+        };
+    },
+
+    handleLogin() {
+        let field = this.state.credentials;
+        const specialCharacter = "."
+        let splitIndex = -1
+        for (let i = 0; i < field.length; i++) {
+            let c = field.charAt(i)
+            if (c === specialCharacter) {
+                splitIndex = i
+                break
+            }
+        }
+        let username = field.substring(0, splitIndex).trim()
+        let password = field.substring(splitIndex + 1).trim()
+
+        if (username === "" || password === "") {
+            this.setState({ messageToUser: "Invalid Input" })
+        }
+        else {
+            let result = null;
+            let query = r.table('users').get(username);
+            ReactRethinkdb.DefaultSession.runQuery(query).then(user => {
+                console.log("user", user)
+                if (user) {
+                    bcrypt.compare(password, user.password, (err, check) => {
+                        if (check) {
+                            result = { user, token: sign(user, secret) }
+                            console.log('login result', result)
+                            sessionStorage.setItem("token", result.token)
+                            sessionStorage.setItem("user_id", result.user.id)
+                            sessionStorage.setItem("role", result.user.role)
+                            this.setState({ messageToUser: "" })
+                        }
+                        else {
+                            this.setState({ messageToUser: "Invalid Input" })
+                        }
+                    })
+
+                    //For now my goal is to be able to create an exam, but this 
+                    //should be in a "Home.js" file that checks who is logged in and 
+                    //depending on the role it will redirect the user to whatever 
+                    //homepage they can are supposed to be in
+                    //if (result.user.role === "Instructor")
+                    //    this.props.history.push("/Instructor")
+                }
+                else {
+                    this.setState({ messageToUser: "Invalid Input" })
+                }
+            });
+        }
+    },
+
+
+    render() {
+        return (
+            <div className='login-container' style={{
+                backgroundImage: 'url(' + photo + ')',
+                backgroundSize: 'cover',
+                overflow: 'hidden',
+            }}>
+                <center>
+                    <div class="uk-card uk-card-default uk-card-body uk-width-1-4@m  login-card" style={{ borderRadius: 20 }}>
+                        <img src={logo} style={{ width: 200, height: 150 }} />
+                        <hr />
+                        <h3 class="login-title"><strong>Username.Password</strong></h3>
+
+                        <div class="uk-margin">
+                            <div class="uk-inline">
+                                <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+                                <input class="uk-input login-input" type="password" placeholder="username.password" />
+                            </div>
+                        </div>
+                        <button class="uk-button login-btn">Login</button>
+                        <br />
+                        <br />
+                        <a href="https://www.w3schools.com" className="login-link">Forgot Your Password?</a>
+
+                    </div>
+                </center>
+
+            </div>
+        )
+    },
+});
+
+export default Login;
