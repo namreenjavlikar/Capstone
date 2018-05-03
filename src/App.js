40271571034c038f3c1bc5883aca46e2eef1b1e7
Eman Hamed <60082612@cna-qatar.edu.qa>
2018-05-03 09:06:39 +0300

    +Add Register to App.js Routers does not work in browser

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import ReactRethinkdb from 'react-rethinkdb'
 import createReactClass from 'create-react-class'
 import Login from './Login'
+import Register from './Register'
 import * as Questions from './Questions'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
@@ -14,6 +15,7 @@ const App = createReactClass({
     return <Router>
       <div>
         <Route path="/" component={Login} />
+        <Route path="/register" component={Register} />
       </div>
     </Router>
   },
